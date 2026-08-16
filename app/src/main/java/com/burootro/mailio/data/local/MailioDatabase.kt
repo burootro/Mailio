9508a01dc8244ba03fde93c22d7175060d22f8bc
burootro <businessroot440@outlook.com>
2026-08-16 18:17:46 +0300

    Create MailioDatabase with Room database setup

---
+++ app/src/main/java/com/burootro/mailio/data/local/MailioDatabase.kt
@@ -0,0 +1,30 @@
+package com.burootro.mailio.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.burootro.mailio.data.local.dao.AddressDao
+import com.burootro.mailio.data.local.dao.AttachmentDao
+import com.burootro.mailio.data.local.dao.MessageDao
+import com.burootro.mailio.data.local.entity.AddressEntity
+import com.burootro.mailio.data.local.entity.AttachmentEntity
+import com.burootro.mailio.data.local.entity.MessageEntity
+
+@Database(
+    entities = [
+        AddressEntity::class,
+        MessageEntity::class,
+        AttachmentEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class MailioDatabase : RoomDatabase() {
+
+    abstract fun addressDao(): AddressDao
+    abstract fun messageDao(): MessageDao
+    abstract fun attachmentDao(): AttachmentDao
+
+    companion object {
+        const val DATABASE_NAME = "mailio.db"
+    }
+}
